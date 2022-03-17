1224a09cd663aa10602ae7b9fbac570776432e08
Herz, Andreas <andreas.herz@sap.com>
2022-03-17 09:35:20 +0100

    no message

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+# Settings in .env have prio
+if [ -f ./configuration/settings.ini ]; then
+  set -o allexport
+  source ./configuration/settings.ini
+  set +o allexport
+fi
+
+
+echo "Using Domain: $DOMAIN"
+
+cat ./yaml/deployment.yaml | sed "s~<DOMAIN>~$DOMAIN~g" | kubectl apply -f -
+cat ./yaml/istio-virtualservice.yaml | sed "s~<DOMAIN>~$DOMAIN~g" | kubectl apply -f -
+cat ./yaml/service.yaml | sed "s~<DOMAIN>~$DOMAIN~g" | kubectl apply -f -
