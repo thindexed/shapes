dd1fa2b35c99b11057190cb682686122cd888714
Herz, Andreas <andreas.herz@sap.com>
2022-03-29 17:42:11 +0200

    no message

--- src/server/index.js
+++ src/server/index.js
@@ -6,6 +6,7 @@ const http = require('http').Server(app)
 const bodyParser = require('body-parser')
 const update = require("./update")
 const {thumbnail} = require("./thumbnail")
+const {listFiles, getJSONFile, getBase64Image } = require("./utils/file")
 const shapesDir = path.normalize(__dirname + '/../shapes/')
 
 const PORT = process.env.PORT || 8080
@@ -17,6 +18,16 @@ console.log(shapesDir)
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 
+function ensureAdminLoggedIn(options) {
+  return function (req, res, next) {
+    let role = req.get("x-role")
+    if ( role !== "admin") {
+      res.send(401, 'string')
+      return
+    }
+    next();
+  }
+}
 
 // =======================================================================
 //
@@ -30,6 +41,21 @@ async function  runServer() {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use('/shapes/global', express.static(shapesDir));
 
+
+  // TODO: migrate to REST service API
+  app.get('/shapes/global/list', (req, res) => listFiles(shapesDir, req.query.path, res))
+  app.get('/shapes/global/get', (req, res) => getJSONFile(shapesDir, req.query.filePath, res))
+  app.get('/shapes/global/image', (req, res) => getBase64Image(shapesDir, req.query.filePath, res))
+  app.post('/shapes/global/delete', ensureAdminLoggedIn(), (req, res) => {
+    module.exports.deleteFile(shapesDir, req.body.filePath)
+    module.exports.deleteFile(shapesDir, req.body.filePath.replace(".shape", ".js"))
+    module.exports.deleteFile(shapesDir, req.body.filePath.replace(".shape", ".md"))
+    module.exports.deleteFile(shapesDir, req.body.filePath.replace(".shape", ".custom"))
+    module.exports.deleteFile(shapesDir, req.body.filePath.replace(".shape", ".png"), res)
+  })
+  app.post('/shapes/global/rename', ensureAdminLoggedIn(), (req, res) => module.exports.renameFile(shapesDir, req.body.from, req.body.to, res))
+  app.post('/shapes/global/folder', ensureAdminLoggedIn(), (req, res) => module.exports.createFolder(shapesDir, req.body.filePath, res))
+  
   app.post('/', (req, res) => {
       let githubPath = req.body.filePath
       let content = req.body.content
