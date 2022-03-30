18af0cb98e2d987a94af5f615b4382d4f605caec
Andreas Herz <a.herz@freegroup.de>
2022-03-30 14:58:01 +0200

    -empty-

--- src/shapes/circuit/74LSXX/74LS90.js
+++ src/shapes/circuit/74LSXX/74LS90.js
@@ -7,7 +7,7 @@
 var circuit_74LSXX_74LS90 = CircuitFigure.extend({
 
    NAME: "circuit_74LSXX_74LS90",
-   VERSION: "2.0.343_1136",
+   VERSION: "local-version",
 
    init:function(attr, setter, getter)
    {
