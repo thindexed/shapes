f5d639146a292ad15192f3a165bde16abffb941e
Herz, Andreas <andreas.herz@sap.com>
2022-03-30 15:29:04 +0200

    no message

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,6 @@ fi
 echo "Using Domain: $DOMAIN"
 
 cat ./yaml/deployment.yaml | sed "s~<DOMAIN>~$DOMAIN~g" | kubectl apply -f -
+cat ./yaml/istio-authpolicy.yaml | sed "s~<DOMAIN>~$DOMAIN~g" | kubectl apply -f -
 cat ./yaml/istio-virtualservice.yaml | sed "s~<DOMAIN>~$DOMAIN~g" | kubectl apply -f -
 cat ./yaml/service.yaml | sed "s~<DOMAIN>~$DOMAIN~g" | kubectl apply -f -
