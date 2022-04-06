bba341a75e555bfa37a4d9606f9f4c97ca607c92
Herz, Andreas <a.herz@freegroup.de>
2022-04-06 22:54:36 +0200

    move data dir into dockerfile scope

--- src/server/configuration.js
+++ src/server/configuration.js
@@ -3,13 +3,13 @@ const path = require("path")
 module.exports = {
 
     absoluteGlobalDataDirectory: () => {
-        let rootPath = process.env.DATA_DIR || (__dirname + "/../../data/")
+        let rootPath = process.env.DATA_DIR || (__dirname + "/../data/")
         return path.normalize(`${rootPath}global/`)
     },
 
     absoluteUserDataDirectory: ( req ) => {
         let hash = req.get("x-hash")
-        let rootPath = process.env.DATA_DIR || (__dirname + "/../../data/")
+        let rootPath = process.env.DATA_DIR || (__dirname + "/../data/")
         return path.normalize(`${rootPath}user/${hash}/`)
     },
 
